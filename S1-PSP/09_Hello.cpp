3f4ff20ff7af9abbe2f9d2a6a4320ccd5f1580c9
katchungaB3367 <katchungablaise@gmail.com>
2025-10-08 19:13:04 +0530

    Completed exercise 9
    Filled in blanks to complete the code and print Hello to console

---
+++ S1-PSP/09_Hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// function that prints the greeting
+void hello() {
+    // prints "Hello" followed by a newline
+    cout << "Hello" << endl;
+}
+
+int main() {
+    // calls the greeting function
+    hello();
+    return 0;
+}
